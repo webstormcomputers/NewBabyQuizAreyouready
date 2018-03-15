5d92fbc20b1d8b8abd215d35700890a0a26b6453
webstormcomputers <aaron@webstormcomputers.com>
2018-03-14 19:18:50 -0500

    layout design, wokring on positioning and appearance, made all questions now show up in the same place. Visibility is set dynamically.

--- app/src/main/java/com/webstormcomputers/newbabyquizareyouready/MainActivity.java
+++ app/src/main/java/com/webstormcomputers/newbabyquizareyouready/MainActivity.java
@@ -1,7 +1,11 @@
 package com.webstormcomputers.newbabyquizareyouready;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void invisible(View v)
+    {
+      TextView dissapear = (TextView) findViewById(R.id.openingText);
+        dissapear.setVisibility(View.GONE);
+        Button next = (Button) findViewById(R.id.Next);
+        next.setVisibility(View.GONE);
+        TextView quest1 = (TextView) findViewById(R.id.ques1Text);
+        quest1.setVisibility(View.VISIBLE);
+        RadioGroup quest1Rad = (RadioGroup) findViewById(R.id.ques1Rad);
+         quest1Rad.setVisibility(View.VISIBLE);
+            }
+
+
 }
